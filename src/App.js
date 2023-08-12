28c88a4ceeaae4fe71087de49633289afab162c7
Rajkumar <92023000+rajkumar-07@users.noreply.github.com>
2023-08-12 22:40:50 +0530

    Temporary Component Skeleton - Product card to show the product data which includes add the current product to the Cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import AddProducts from "./Components/AddProducts";
+import ProductCard from "./Components/ProductCard";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
-      <Route path ="/addProducts" element={<AddProducts/>}/>
+      <Route path ="/add-products" element={<AddProducts/>}/>
+      <Route path="/product-list" element={<ProductCard/>}/>
       </Routes>
       </Router>
   );
