830563096b30c3df0d370b4b3a6f3345485d7f7a
Rajkumar <92023000+rajkumar-07@users.noreply.github.com>
2023-08-14 22:01:55 +0530

    Code refactoring

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,28 +1,47 @@
 import React from 'react';
-import "./productcard.css";
+import "./componentStyles/productcard.css";
 import tempProduct from "../Assets/Product.jpg";
 
-function ProductCard() {
+function ProductCard(props) {
+    // const {
+    //     productName,
+    //     productDesc,
+    //     quantity,
+    //     price
+    // } =this.props;
     return (
         <div className="product-container-outer-layer">
             <table className="product-container">
                 <tr>
                     <td className="product-image"><img src={tempProduct} alt="Product"/></td>
-                    <td>
+                    <td className="product-details">
                         <p className="product-name">Product name</p>
                         <p className="product-description">Description</p>
-                        <p>Quantity</p>
-                        <p>Price</p>
+                        <p className="product-quantity">Quantity</p>
+                        <p className="product-price">Price</p>
                     </td>
+                    <br/>
                 </tr>
-                
                 <tr>
-                    <td colSpan="2">
-                        <input className="input-field" placeholder="Quantity" />
+                    <td>
+                    </td>
+                    <td>
+                    <td >
+                        <input 
+                        className="quantity-input-field" 
+                        placeholder="Quantity" 
+                        type="number"
+                        />
                         <div className="action-buttons">
-                            <button className="action-button">Add to Cart</button>
-                            <button className="action-button">View Product</button>
+                            <button 
+                            className="action-button">
+                                Add to Cart
+                            </button>
+                            <button className="action-button">
+                                View Product
+                            </button>
                         </div>
+                    </td>
                     </td>
                 </tr>
             </table>
