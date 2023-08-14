67ba749139dde1fd78035beff343c7f1b3f8872e
Rajkumar <92023000+rajkumar-07@users.noreply.github.com>
2023-08-14 22:31:16 +0530

    Get products limit 10 by default - temporary

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,6 +1,29 @@
+import { useEffect } from "react";
 import ProductCard from "./ProductCard";
 
 function ProductList(){
+    const getProductAPI = async () => {
+        try {
+          const response = await fetch("http://localhost:8080/get-products?pageNo=1&limit=10", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+    
+          if (response.ok) {
+            const data = await response.json();
+            console.log(data); // Display the response data
+          } else {
+            console.log("Response not okay:", response.status, response.statusText);
+          }
+        } catch (error) {
+          console.log("Error:", error);
+        }
+      }
+    useEffect(()=>{
+        getProductAPI();
+    })
     return(
         <div>
             <ProductCard/>
